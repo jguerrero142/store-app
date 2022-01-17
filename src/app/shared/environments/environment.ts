30f70a9bf79a9e51b7fc47600ff6ddf2427e2e97
jguerrero142 <julgueover.05@gmail.com>
2022-01-17 16:47:41 -0500

    producs

--- src/app/shared/environments/environment.ts
+++ src/app/shared/environments/environment.ts
@@ -15,3 +15,87 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(9, 'Frijolada', 5000, '2021-09-26 13:59:19', 'https://caldoparao.com/wp-content/uploads/2020/06/MG_1162.frijolada-jpg.jpg', 'Deliciosa frijolada con chorizo ', 1, 1, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(10, 'Pollo asado', 5000, '2021-09-26 13:59:51', 'https://thumbs.dreamstime.com/b/almuerzo-con-el-pollo-asado-la-parrilla-filete-60175333.jpg', 'Deliciosa pollo asado', 1, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(11, 'Arroz Mixto', 5000, '2021-09-26 14:00:44', 'https://i2.wp.com/decoriente.co/wp-content/uploads/2017/12/ARROZ-MIXTO.jpg?resize=600%2C600', 'Delicioso arroz mixto con sopa', 1, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(12, 'Arepa con huevo', 5000, '2021-09-26 14:02:09', 'https://arepas.top/wp-content/uploads/2018/11/arepa-de-huevo.jpg', 'Delicioso arepa con huevo', 2, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(13, 'calentado', 5000, '2021-09-26 14:02:33', 'http://farm8.staticflickr.com/7336/9763700186_33927c3934_c.jpg', 'Delicioso calentado de frijoles', 2, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(14, 'empanada', 1000, '2021-09-26 14:03:41', 'https://4.bp.blogspot.com/-C-PxcAwhHXo/WlWDzAmHB7I/AAAAAAAAG8k/hblgAA_HrIIYW_VPzVNuXb7ZjFnxKpduwCLcBGAs/s1600/como-hacer-empanadas-venezolanas.jpg', 'Delicioso empanada de carne', 2, 1, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(15, 'bunuelos', 1000, '2021-09-26 14:03:57', 'https://www.gourmet.com.co/wp-content/uploads/2021/02/bun%CC%83uelos.jpg', 'Delicioso bunuelo', 2, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(16, 'cono de nucita', 1000, '2021-09-26 14:04:53', 'https://www.valesupermercado.com/wp-content/uploads/2020/04/003_min.jpg', 'Delicioso cono de nucita', 5, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(17, 'paleta tentacion', 1000, '2021-09-26 14:05:23', 'https://www.valesupermercado.com/wp-content/uploads/2020/04/002_min.jpg', 'Delicioso paleta tentacion', 5, 1, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(18, 'paleta de agua', 1000, '2021-09-26 14:05:56', 'https://www.deliciosi.com/images/2700/2715/paletas-de-helados.jpg', 'Delicioso paleta agua', 5, 1, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(19, 'gaseosa', 1500, '2021-09-26 14:03:57', 'https://cdni.rt.com/actualidad/public_images/2016.02/article/r/1104/90/jpeg/56afe82bc46188be6a8b4606.jpg', 'Refrescante bebida', 3, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(20, 'vive 100', 1000, '2021-09-26 14:03:57', 'https://jumbocolombiafood.vteximg.com.br/arquivos/ids/155810-1000-1000/7702354929503-1.jpg?v=636066426090630000', 'Refrescante bebida', 3, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(21, 'pony malta', 1500, '2021-09-26 14:03:57', 'https://www.merkofacil.com/wp-content/uploads/2020/07/Pony-malta-x330.jpg', 'Refrescante bebida', 3, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(22, 'speed max', 1200, '2021-09-26 14:03:57', 'https://jumbocolombiafood.vteximg.com.br/arquivos/ids/171638-1000-1000/7702090039214-20-281-29.jpg?v=636111275378930000', 'Refrescante bebida', 3, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(23, 'heinneken', 1800, '2021-09-26 14:03:57', 'https://puntacamaron.com.co/165/cerveza-heineken.jpg', 'Refrescante bebida', 3, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(24, 'agua en botella', 1000, '2021-09-26 14:03:57', 'https://minesur.com.ar/wp-content/uploads/2019/04/producto-botella-1.5litros.jpg', 'Refrescante bebida', 3, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(25, 'Cheese Tris', 1200, '2021-09-26 14:03:57', 'https://larebajavirtual.com/images/productos/sii/F/300x300/cheese_tris-61369-1627486874.png', 'Delicioso Cheese Tris', 4, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(26, 'Jumbo Mix', 1000, '2021-09-26 14:03:57', 'https://cdn.shopify.com/s/files/1/0492/2458/1274/products/supermercados_la_vaquita_supervaquita_chocolatina_jumbo_mix_60g_dulceria_700x700.jpg?v=1626786355', 'Delicioso chocolatina', 4, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(27, 'Boliqueso', 1000, '2021-09-26 14:03:57', 'https://jumbocolombiafood.vteximg.com.br/arquivos/ids/3706942-1000-1000/7702189045577.jpg?v=637517720947330000', 'Delicioso Boliqueso', 4, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(28, 'Bom bom bum', 400, '2021-09-26 14:03:57', 'https://static.wixstatic.com/media/9ae4c7_d08328dfd3624e74bca981bfbfaeec9a~mv2.jpeg/v1/fill/w_980,h_332,al_c,q_85,usm_0.66_1.00_0.01/9ae4c7_d08328dfd3624e74bca981bfbfaeec9a~mv2.jpeg', 'Delicioso Bom bom bum', 4, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(29, 'Detodito', 2000, '2021-09-26 14:03:57', 'https://www.eurosupermercados.com.co/media/catalog/product/cache/5db2c2cd17d64c361c13f6f6460f1cf1/7/7/7702189055040.jpg', 'Delicioso Detodito', 4, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(30, 'vasito vainilla', 1000, '2021-09-26 14:03:57', 'https://www.heladoslafresita.com.co/wp-content/uploads/2018/04/vasito-vainilla-800x560_c.jpg', 'Delicioso vasito vainilla', 5, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(31, 'fruly mora', 1000, '2021-09-26 14:03:57', 'https://www.valesupermercado.com/wp-content/uploads/2020/04/85.jpg', 'Delicioso fruly mora', 5, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(32, 'cono tres leches', 1000, '2021-09-26 14:03:57', 'https://www.valesupermercado.com/wp-content/uploads/2020/04/013_min.jpg', 'cono tres leches', 5, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(33, 'pan queso', 1000, '2021-09-26 14:03:57', 'https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2019/02/24212941/RFB-1502-5-pandequeso.jpg', 'Delicioso pan queso', 2, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(34, 'papa aborrajada', 1000, '2021-09-26 14:03:57', 'https://guiagastronomica.co/wp-content/uploads/Papa-aborrajada.jpg', 'Delicioso bunuelo', 2, 0, 1);
+// INSERT INTO app_store.producto
+// (id, name, valor, create_at, image, descripcion, producto_tipo, menu, active)
+// VALUES(38, 'Pescado', 5000, '2021-09-26 14:00:44', 'https://pescado.site/wp-content/uploads/2021/06/filete-de-pescado-frito-con-arroz-y-ensalada.jpg', 'Delicioso bocachico con sopa', 1, 0, 1);
+
